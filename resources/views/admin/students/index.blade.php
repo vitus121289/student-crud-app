377b225c80e52ded3f730a854ee5d013a8308eec
vitus121289 <vmtusing@gmail.com>
2022-09-05 14:40:24 +0800

    Edited the admin index page to show all the students.

--- resources/views/admin/students/index.blade.php
+++ resources/views/admin/students/index.blade.php
@@ -1,5 +1,34 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 @extends('layout')
 @section('content')
-    <x-navbar />
+    <h1>This is the index page.</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Address</th>
+                <th colspan="2">
+                    Actions
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($students as $student)
+                <tr>
+                    <td>
+                        {{ $student->first_name . ' ' . $student->middle_name . ' ' . $student->last_name }}
+                    </td>
+                    <td>
+                        {{ $student->address }}
+                    </td>
+                    <td>
+                        <a href="/admin/students/{{ $student->id }}/edit">Edit</a>
+                    </td>
+                    <td>
+                        <a href="#">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
