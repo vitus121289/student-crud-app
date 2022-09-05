7a44ea4212244cf06a5051fa374a26dde5d92236
vitus121289 <vmtusing@gmail.com>
2022-09-05 22:17:56 +0800

    Created a button blade component.

---
+++ resources/views/components/forms/button.blade.php
@@ -0,0 +1,5 @@
+<button
+    class="w-full rounded-2xl border-b-4 border-b-blue-600 bg-blue-500 py-3 font-bold text-white hover:bg-blue-400 active:translate-y-[0.125rem] active:border-b-blue-400"
+>
+    {{ $slot }}
+</button>
