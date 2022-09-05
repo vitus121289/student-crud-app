ff8ae361034d4fbb2400e4e269034e462fd7a5ea
vitus121289 <vmtusing@gmail.com>
2022-09-05 14:44:31 +0800

    Edited to create the create, store, edit, update, and destroy routes for the admin/students.

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,8 @@ use App\Http\Controllers\Admin\StudentController as AdminStudentController;
 */
 
 Route::get('admin/students', [AdminStudentController::class, 'index']);
+Route::get('admin/students/create', [AdminStudentController::class, 'create']);
+Route::post('admin/students/store', [AdminStudentController::class, 'store']);
+Route::get('admin/students/{student}/edit', [AdminStudentController::class, 'edit']);
+Route::patch('admin/students/{student}/', [AdminStudentController::class, 'update']);
+Route::delete('admin/students/{student}', [AdminStudentController::class, 'destroy']);
