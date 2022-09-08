57cf9383fe79d29019d7d39a1e652853e302a280
vitus121289 <vmtusing@gmail.com>
2022-09-08 15:15:07 +0800

    Added the capability to retain value entered by the user when validation fails.

--- resources/views/admin/students/create.blade.php
+++ resources/views/admin/students/create.blade.php
@@ -3,13 +3,13 @@
     <x-forms.section heading="Add Student">
         <form action="/admin/students/store" method="post" enctype="multipart/form-data">
             @csrf
-            <x-forms.input name="first_name" type="text" required/>
-            <x-forms.input name="middle_name" type="text" required/>
-            <x-forms.input name="last_name" type="text" required/>
-            <x-forms.input name="birthdate" type="date" required/>
+            <x-forms.input name="first_name" type="text" value="{{ old('first_name') }}" required />
+            <x-forms.input name="middle_name" type="text" value="{{ old('middle_name') }}" required/>
+            <x-forms.input name="last_name" type="text" value="{{ old('last_name') }}" required/>
+            <x-forms.input name="birthdate" type="date" value="{{ old('birthdate') }}" required/>
             <x-forms.input name="photo" type="file"/>
-            <x-forms.textarea name="section"></x-forms.textarea>
-            <x-forms.textarea name="address" required></x-forms.textarea>
+            <x-forms.textarea name="section">{{ old('section') }}</x-forms.textarea>
+            <x-forms.textarea name="address" required>{{ old('address') }}</x-forms.textarea>
             <x-forms.button>Add Student</x-forms.button>
         </form>
     </x-forms.section>
