61ab170e3eba6210a0d4f7e7af3884097e98ea68
vitus121289 <vmtusing@gmail.com>
2022-09-08 15:12:43 +0800

    Required the route file for admin/students

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,4 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+require __DIR__.'/admin/students.php';
