307e487e681ea417aac973135d6dce97d7405d68
vitus121289 <vmtusing@gmail.com>
2022-09-12 17:01:20 +0800

    Edited to use the published css and js that came with the installation of breeze.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     <title>Argon Software: Student Crud App</title>
 </head>
 <body>
