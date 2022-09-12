c9900e3c95b5858d507498cd0af700e3581f547e
vitus121289 <vmtusing@gmail.com>
2022-09-12 16:56:09 +0800

    Updated so that the routes here are only accessible if the logged-in user is an admin.

--- routes/admin/students.php
+++ routes/admin/students.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\StudentController as AdminStudentController;
 
-Route::get('admin/students', [AdminStudentController::class, 'index']);
-Route::get('admin/students/create', [AdminStudentController::class, 'create']);
-Route::get('admin/students/{student}/edit', [AdminStudentController::class, 'edit']);
-Route::post('admin/students/store',[AdminStudentController::class, 'store']);
-Route::patch('admin/students/{student}', [AdminStudentController::class, 'update']);
-Route::delete('admin/students/{student}', [AdminStudentController::class, 'destroy']);
+Route::middleware('can:admin')->group(function() {
+    Route::get('admin/students', [AdminStudentController::class, 'index']);
+    Route::get('admin/students/create', [AdminStudentController::class, 'create']);
+    Route::get('admin/students/{student}/edit', [AdminStudentController::class, 'edit']);
+    Route::post('admin/students/store',[AdminStudentController::class, 'store']);
+    Route::patch('admin/students/{student}', [AdminStudentController::class, 'update']);
+    Route::delete('admin/students/{student}', [AdminStudentController::class, 'destroy']);
+});
